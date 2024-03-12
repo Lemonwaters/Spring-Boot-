1313176b94ccd5bef1b692139bac3cd872fb39e1
lixisheng <3180755859@qq.com>
2024-03-12 21:08:24 +0800

    2024/3/12 简书页面制作

---
+++ spring-boot-bootstrap/src/main/java/edu/niit/boot/bootstrap/BootstrapApplication.java
@@ -0,0 +1,15 @@
+package edu.niit.boot.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 31807
+ */
+
+@SpringBootApplication
+public class BootstrapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapApplication.class, args);
+    }
+}
